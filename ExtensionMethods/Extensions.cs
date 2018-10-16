1b49ba0e54aac862793f9f09a0fe6df0231ea5ed
nguyhai <tenth11@gmail.com>
2018-10-15 22:12:54 -0500

    Add project files.

---
+++ ExtensionMethods/Extensions.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionMethods
+{
+    public static class Extensions
+    {
+        public static void Sort(this int[] array)
+        {
+            int temp = 0;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = 0; j < array.Length - 1; j++)
+                {
+                    if (array[j] > array[j+1])
+                    {
+                        temp = array[j + 1];
+                        array[j + 1] = array[j];
+                        array[j] = temp;
+                    }
+                }
+            }
+        }
+    
+        public static void Sort(this int[] array, bool reverse = false)
+        {
+            array.Sort();
+
+            if (reverse)
+            {
+                Array.Reverse(array);
+            }
+        }
+
+        public static string ReverseString(this string str)
+        {
+            string reversedWord = "";
+
+            for (int i = 1; i <= str.Length; i++)
+            {
+                reversedWord += str.Substring(str.Length - i, 1);
+            }
+
+            return reversedWord;
+        }
+
+    }
+}
---
+++ ExtensionMethods/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 1, 3, 5, 76, 21, 3, 5, 6, 2, 56, 246, 26, 2, 5, 2, 5, 3, 35, 23 };
+            string str = "Hello There Mang";
+            string reversedString = "";
+
+            array.Sort();
+            reversedString = str.ReverseString();
+
+            Console.WriteLine(reversedString);
+
+
+        }
+    }
+}
